1b4b5bfc56851dc61f539d38dc716e3e17b6cb81
Alexandr Kotov <sa.cotov@yandex.ru>
2025-06-18 01:04:49 +0300

    feat: add validate field in create request

--- internal/handlers/http/registered_services/payload.go
+++ internal/handlers/http/registered_services/payload.go
@@ -1,9 +1,9 @@
 package registered_services
 
 type CreateRequest struct {
-	Name        string   `json:"name"`
+	Name        string   `json:"name" validate:"required,min=3,max=128"`
 	Description string   `json:"description"`
-	HealthUrl   string   `json:"health_url"`
-	OwnerEmail  string   `json:"owner_email"`
+	HealthUrl   string   `json:"health_url" validate:"required,url"`
+	OwnerEmail  string   `json:"owner_email" validate:"required,email"`
 	Tags        []string `json:"tags"`
 }
