0d7ed2dceb9842ec328c4007393fe414b9ccc49f
Alexandr Kotov <sa.cotov@yandex.ru>
2025-06-18 00:40:51 +0300

    refactor: edit service model

--- internal/domain/services/model.go
+++ internal/domain/services/model.go
@@ -5,16 +5,21 @@ import (
 	"time"
 )
 
+const (
+	Healthy   = "Healthy"
+	Unhealthy = "Unhealthy"
+)
+
 type RegisteredServices struct {
-	ID             uint `gorm:"primarykey"`
-	Name           string
-	Description    string
-	HealthCheckUrl string
-	OwnerEmail     string
-	Tags           []string
-	Status         string
+	ID             uint     `gorm:"primarykey"`
+	Name           string   `gorm:"varchar(128)"`
+	Description    string   `gorm:"text"`
+	HealthCheckUrl string   `gorm:"varchar(128)"`
+	OwnerEmail     string   `gorm:"varchar(128)"`
+	Tags           []string `gorm:"column:tags;type:jsonb;serializer:json"`
+	Status         string   `gorm:"varchar(10)"`
 	LastCheckedAt  time.Time
 	CreatedAt      time.Time
 	UpdatedAt      time.Time
-	DeletedAt      gorm.DeletedAt `gorm:"index"`
+	DeletedAt      gorm.DeletedAt `gorm:"uniqueIndex"`
 }
