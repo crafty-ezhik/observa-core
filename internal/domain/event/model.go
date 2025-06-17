14b0fe558427db6b0db76266ec0d1f3c63e957cd
Alexandr <alexandr@biznes-len.com>
2025-06-17 18:00:26 +0300

    feat: add model for event and subscription

--- internal/domain/event/model.go
+++ internal/domain/event/model.go
@@ -1,1 +1,13 @@
 package event
+
+import (
+	"gorm.io/gorm"
+	"time"
+)
+
+type Event struct {
+	ID        uint `gorm:"primarykey"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+}
--- internal/domain/subscription/model.go
+++ internal/domain/subscription/model.go
@@ -1,1 +1,13 @@
 package subscription
+
+import (
+	"gorm.io/gorm"
+	"time"
+)
+
+type Subscription struct {
+	ID        uint `gorm:"primarykey"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+}
